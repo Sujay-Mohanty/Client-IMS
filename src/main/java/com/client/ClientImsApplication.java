673b190f1ab29d4198b4765cbc6dd1ac21c82c07
Sujay-Mohanty <sujaymohanty2001@gmail.com>
2025-05-15 11:39:06 +0000

    First commit Client

---
+++ src/main/java/com/client/ClientImsApplication.java
@@ -0,0 +1,13 @@
+package com.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientImsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClientImsApplication.class, args);
+	}
+
+}
